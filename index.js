e99b2de9c210fd33d8b8cc92023704d67fdb0267
Shubh-i-am-s <whyratkohlii18@gmail.com>
2026-01-04 14:07:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const inputCity = document.getElementById("inputCity");
+const ciytsearch = document.getElementById("ciytsearch");
+const cityName = document.getElementById("cityName");
+const temp = document.getElementById("temp");
+const weatherEmoji = document.getElementById("weatherEmoji");
+const condition = document.getElementById("condition");
+const msgError = document.getElementById("msgError");
+
+
+
+
+ciytsearch.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const apiKey = "10dd25667165ae823fe9cd2b3630c1e3";
+    let city = inputCity.value.trim();
+    if (city === "") {
+    alert("Please enter a city name");
+    return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try{
+        const response = await fetch(url);
+
+        if(!response.ok){
+            throw new Error("NO  response fetched from API");
+        }
+        const data = await response.json();
+
+        
+        cityName.innerText = `City: ${data.name}`;
+        temp.innerText = `Temperature: ${Math.floor(data.main.temp)}°C`;
+
+        const emoji = data.weather[0].id;
+
+        if(emoji >= 200 && emoji < 300){
+            weatherEmoji.innerText = "⛈️";
+        }else if(emoji >= 300 && emoji < 550){
+            weatherEmoji.innerText = "🌧️";
+        }else if(emoji >= 550 && emoji < 701){
+            weatherEmoji.innerText = "🌨️";
+        }else if(emoji >= 701 && emoji < 800){
+            weatherEmoji.innerText = "🍃";
+        }else if(emoji == 800){
+            weatherEmoji.innerText = "🌤️";
+        }else{
+            weatherEmoji.innerText = "☁️";
+        }
+        condition.innerText = `Description: ${data.weather[0].main}`;
+        
+    }catch(error){
+        msgError.innerText = error;
+
+    }
+    inputCity.value = "";
+});
+
